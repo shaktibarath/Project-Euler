98246673c94397697696e610e65f468681f8a08d
shaktibarath <shakti.barath@gmail.com>
2017-11-13 14:08:58 -0500

    v02

--- 01.py
+++ 01.py
@@ -3,7 +3,7 @@ Project Euler problem set 01 implementation.
 Problem 1: If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 
-Author: Shakti Barath <shakti.barath@gmail.com>
+Author: Shakti Barath <shakti.barath[at]gmail.com>
 '''
 total_sum = 0
 for i in range(1, 1000):

716f7607a250169e3f30c2b532e5a83f343f6e14
shaktibarath <shakti.barath@gmail.com>
2017-11-13 13:44:24 -0500

    Logic modified 02.py

--- 02.py
+++ 02.py
@@ -5,3 +5,18 @@
 # find the sum of the even-valued terms.
 
 # Author: Shakti Barath <shakti.barath@gmail.com>
+
+LIMIT = 4*10**6
+even_sum = 0
+def fibonacci(n):
+    if(n == 1 or n == 2):
+        return 1
+    else:
+        return (fibonacci(n-1) + fibonacci(n-2))
+
+i = 1
+while fibonacci(i) <= LIMIT:
+    if fibonacci(i) % 2 == 0:
+        even_sum += fibonacci(i)
+    i += 1
+print(even_sum)

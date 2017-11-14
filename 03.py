7aa4eca1f0ab202db5920979079b51d0a960023a
shaktibarath <shakti.barath@gmail.com>
2017-11-14 08:20:00 -0500

    v03

--- 03.py
+++ 03.py
@@ -1,7 +1,8 @@
 '''
+Largest prime factor:
 The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
-Author: Shakti Barath <shakti.barath@gmail.com>
+Author: Shakti Barath <shakti.barath[at]gmail.com>
 
 Algorithm used:  Sieve of Eratosthenes
 '''
